3c5dfdf854c1b43be59d759fb0daacf94607c77b
Accesito24 <brayandavidg24@gmail.com>
2025-12-17 20:49:15 +0100

    17-12-25

---
+++ ejercicios/src/Examen/Tabla.java
@@ -0,0 +1,32 @@
+package Examen;
+
+import java.util.Scanner;
+
+public class Tabla {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduce el número de filas y columnas: : ");
+        int FC = sc.nextInt();
+
+
+
+        for (int f = 1; f <= FC; f++) {
+            for (int c = 1; c <= FC; c++) {
+
+                if (f == c) {
+                    System.out.print("X\t");
+                } else if (f > c) {
+                    System.out.print((f + c) + "\t");
+                } else if (f < c) {
+                    System.out.print((f - c) + "\t");
+                }
+
+
+            }
+            System.out.println();
+
+        }
+        sc.close();
+    }
+}
