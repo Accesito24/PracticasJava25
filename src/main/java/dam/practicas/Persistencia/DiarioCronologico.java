65fab4fce5bc4ab1671a83076b444b0c41d9394f
Acceso <brayandavidg24@gmail.com>
2026-04-15 20:46:37 +0200

    15/04/2026

---
+++ src/main/java/dam/practicas/Persistencia/DiarioCronologico.java
@@ -0,0 +1,17 @@
+package dam.practicas.Persistencia;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.DateTimeException;
+import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
+
+public class DiarioCronologico {
+    public static void main() {
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyy HH:mm:ss" );
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
---
+++ src/main/java/dam/practicas/Persistencia/NotarioDigital.java
@@ -0,0 +1,34 @@
+import java.io.FileWriter;
+import java.io.IO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public static void main(String[] args) {
+    PrintWriter escritor = null;
+    Scanner sc = new Scanner(System.in);
+
+    try {
+        escritor = new PrintWriter(new FileWriter("registro.txt", false));
+
+        while (true) {
+            System.out.println("Escribe una frase o escribe FIN");
+            String frase = sc.nextLine();
+
+            if (frase.equalsIgnoreCase("FIN")) {
+                break;
+            } else {
+                escritor.println(frase);
+            }
+        }
+
+    } catch (IOException e) {
+        System.out.println("Error: " + e.getMessage());
+    } finally {
+        if (escritor != null) {
+            escritor.close();
+        }
+        sc.close();
+        System.out.println("Programa finalizado");
+    }
+}
